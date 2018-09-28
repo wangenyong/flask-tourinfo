58a29159bac60b2e9b9f0cf9766cbc7253b6c6c7
wangenyong <wangenyong@gmail.com>
2018-09-28 14:44:13 +0800

    配置数据库升级

--- flasky.py
+++ flasky.py
@@ -1,7 +1,14 @@
 import os
-from flask_migrate import Migrate
+from flask_migrate import Migrate, upgrade
 from app import create_app, db
 
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
+
+
+@app.cli.command()
+def deploy():
+    """Run deployment tasks."""
+    # migrate database to latest revision
+    upgrade()
